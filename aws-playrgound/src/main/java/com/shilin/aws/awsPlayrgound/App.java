20a3aba12dd23a766d14b0e84e97e4f76c3850b3
shilin-gan <shilin_gan@gallup.com>
2016-08-17 11:23:40 -0500

    maven starter

---
+++ aws-playrgound/src/main/java/com/shilin/aws/awsPlayrgound/App.java
@@ -0,0 +1,13 @@
+package com.shilin.aws.awsPlayrgound;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
